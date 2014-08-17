83dc1495a3e1b89f004ee7ef79aded0b1cb514f1
Daisuke Matsumoto <daikonbuu@gmail.com>
2014-08-18 00:23:21 +0900

    Implemented beta ui

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,76 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+.controller('AppCtrl', function($scope, $http) {
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  $scope.senddata = {};
+  $scope.senddata.hostname = '192.168.1.13';
+//  $scope.senddata.message = {format:"raw",freq:38,data:[18031,8755,1190,1190,1190,3341,1190,3341,1190,3341,1190,1190,1190,3341,1190,3341,1190,3341,1190,3341,1190,3341,1190,3341,1190,1190,1190,1190,1190,1190,1190,1190,1190,3341,1190,3341,1190,1190,1190,3341,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,1190,3341,1190,3341,1190,3341,1190,3341,1190,3341,1190,65535,0,9379,18031,4400,1190]};
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
+        $scope.senddata.message  = '{"format":"raw","freq":38,"data":[6530,3297,825,867,823,867,822,2504,825,866,824,2502,873,819,871,820,871,819,870,2455,874,2454,874,818,871,819,872,818,871,2454,875,2453,875,817,873,817,872,818,873,817,873,818,872,818,872,818,872,818,873,817,873,817,873,817,873,817,873,818,873,2453,875,817,873,817,873,817,873,817,873,817,873,817,873,817,874,2452,876,816,873,817,873,818,873,817,873,2453,875,816,874,816,874,816,874,816,874,817,873,817,873,2452,877,815,874,2453,876,2450,877,2451,877,2451,876,2451,877,2450,877]}';
+
+        $scope.postOut = {};
+
+        // Perform the login action when the user submits the login form
+  $scope.sendData = function() {
+      if (! $scope.senddata.hostname) {
+          alert( "fill hostname:" + $scope.senddata.hostname );
+          return false;
+      }
+      if (! $scope.senddata.message) {
+          alert( "fill Signal as JSON" );
+          return false;
+      }
+
+      console.log('access connect hostname:'+$scope.senddata.hostname+' message:'+$scope.senddata.message);
+     // connectIrkit.save({hostname:$scope.senddata.hostname})
+
+      var url = 'http://'+ $scope.senddata.hostname +'/messages';
+
+   /*   $http({
+          method: 'post',
+          url: 'http://' + $scope.senddata.hostname + '/messages',
+          data: JSON.parse($scope.senddata.message),
+          headers: {'Content-Type': 'application/json'}
+      }).success(function(data, status, headers, config) {
+          console.log('success: status:'+status);
+      }).error(function(data, status, headers, config) {
+          console.log('error: status:'+status);
+      });
+
+*/
+
+
+      $http.post(url,  JSON.parse($scope.senddata.message)
+      ).success(function(data, status, headers, config) {
+              $scope.postOut.status = status;
+              $scope.postOut.data = data;
+              console.log('success: status:'+status);
+          }).error(function(data, status, headers, config) {
+                         $scope.postOut.status = 'error:' + status;
+
+                         $scope.postOut.data = 'error:' + data;
+              console.log('error: status:'+status);
+          });
+
   };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+  $scope.getData = function() {
+      if (! $scope.senddata.hostname) {
+          alert( "fill hostname:" + $scope.senddata.hostname );
+          return false;
+      }
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+      console.log('access connect hostname:'+$scope.senddata.hostname+' message:'+$scope.senddata.message)
+      // connectIrkit.save({hostname:$scope.senddata.hostname})
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+      var url = 'http://'+ $scope.senddata.hostname +'/messages';
+      $http.get(url).success(function(data, status, headers, config) {
+              console.log('success: status:'+status + " data:"+data + " headers:"+headers + "config:"+config);
+
+          }).error(function(data, status, headers, config) {
+              console.log('error: status:'+status);
+          });
+  }
+
+
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
